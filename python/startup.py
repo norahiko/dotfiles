dcdb37067a9f0d27ac137cbca655644ccefc4364
norahiko <norahiko.o@gmail.com>
2013-11-14 02:56:59 +0900

    Initial commit

---
+++ python/startup.py
@@ -0,0 +1,8 @@
+import sys, __main__, os, re, requests, datetime, itertools
+__main__.sys = sys
+__main__.os = os
+__main__.re = re
+__main__.requests = requests
+__main__.datetime = datetime.datetime
+__main__.itertools = itertools
+
